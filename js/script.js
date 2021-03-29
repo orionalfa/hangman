c796d75a3114dc86762dd55434dae20a3ed2d92d
orionalfa <orionalfacentauri@gmail.com>
2021-03-29 12:16:25 +0200

    function prototypes

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+var letterBoxElements = [];
+var wordsArray = [];
+
+function isLetterInWordAndWhere(letter,word){
+    hasInPositions=""; //index array[0] false is letter not in word
+    //else array[0]=true and the positions of letters in string
+    return array;
+}
+
+function displayMikePiece(){
+
+}
+
+function displayLetterInPositions(letter, positions, letterBoxElements){
+
+}
+
+function buildRoomForWord(wordLength){
+    var letterBoxesList = [];
+    return letterBoxesList;
+}
+
+/*
+function accessToSelectedWord(){
+    var level = 1;
+    word ='';
+    return word;
+}
+*/
+
+function randomWordSelector(max,min){
+    return Math.random() * (max - min) + min;
+}
+
+
+
+
+
